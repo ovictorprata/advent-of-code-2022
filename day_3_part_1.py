06b43a193178eb8c43a4342f51afb37ae46f0084
victor.prata <victorsousaprata@gmail.com>
2023-05-02 08:18:24 -0300

    incompleted solution day3

---
+++ day_3_part_1.py
@@ -0,0 +1,85 @@
+input = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+input = input.split('\n')
+letter_value = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26,
+    'A': 27,
+    'B': 28,
+    'C': 29,
+    'D': 30,
+    'E': 31,
+    'F': 32,
+    'G': 33,
+    'H': 34,
+    'I': 35,
+    'J': 36,
+    'K': 37,
+    'L': 38,
+    'M': 39,
+    'N': 40,
+    'O': 41,
+    'P': 42,
+    'Q': 43,
+    'R': 44,
+    'S': 45,
+    'T': 46,
+    'U': 47,
+    'V': 48,
+    'W': 49,
+    'X': 50,
+    'Y': 51,
+    'Z': 52
+}
+soma = 0
+items = []
+print(input)
+
+def split_string(word):
+    half_size = int(len(word) / 2)
+    first_half = word[0:half_size]
+    second_half = word[half_size:(len(word)+1)]
+
+    for letter in first_half:
+        if letter in second_half:
+            return letter
+        
+
+def get_value(value):
+    return letter_value.get(value)
+
+
+items = list(map(split_string, input))
+result = list(map())
+print(letter_value.get('a'))
+            
+    
